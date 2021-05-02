41dff173b41c94332fc42182fcaceb5d6cd4188d
SpeedSterKawaii <72403623+SpeedSterKawaii@users.noreply.github.com>
2021-05-02 19:37:16 -0400

    Update thunder.lua

--- thunder.lua
+++ thunder.lua
@@ -1,1 +1,13 @@
+if game.PlaceId == 2377868063 then
+  game.Players.LocalPlayer:Kick("You have a high change of getting banned.")
+  else
+  wait(1)
+end
 
+if getfenv then 
+  wait(0.30)
+  else
+  warn("Error while doing getfenv")
+end
+
+info("Thunder has Injected")
