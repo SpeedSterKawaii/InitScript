205e21074b9337d88e53615a737623820be29f91
SpeedSterKawaii <72403623+SpeedSterKawaii@users.noreply.github.com>
2021-05-19 10:13:24 -0400

    Update thunder.lua

--- thunder.lua
+++ thunder.lua
@@ -15,6 +15,33 @@ if game.PlaceId == 292439477 then
   wait(0.30)
 end
 
+------ WATERMARK ------
+
+local Effect = Instance.new("BlurEffect")
+Effect.Parent = game.Lighting
+
+local ThunderLoading = Instance.new("ScreenGui")
+local Logo = Instance.new("ImageLabel")
+
+ThunderLoading.Name = "ThunderLoading"
+ThunderLoading.Parent = game.CoreGui
+ThunderLoading.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Logo.Name = "Logo"
+Logo.Parent = ThunderLoading
+Logo.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Logo.BackgroundTransparency = 1.000
+Logo.Position = UDim2.new(0.427343756, 0, 0.396386832, 0)
+Logo.Size = UDim2.new(0, 186, 0, 194)
+Logo.Image = "rbxassetid://6764952705"
+Logo.ScaleType = Enum.ScaleType.Fit
+wait(3)
+for i = 1, 10 do
+    Logo.ImageTransparency = Logo.ImageTransparency + 0.1
+    Effect:Destroy()
+    wait()
+end
+
 ------ GAME TELEPORTER ------
 
 game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(state) 
